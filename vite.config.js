cd7ca909390fa12e81e1d2280137c1fe901120ca
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-08-17 22:48:39 -0500

    FIX BUG

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://gndx.github.io/',
+  base: 'https://gndx.github.io/gh-react-vite/',
   plugins: [react()]
 })
